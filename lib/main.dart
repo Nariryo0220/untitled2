fccfadc0b62fdd87131bf5528ec683ff38c797a7
narir <si19048@edu.aomori-u.ac.jp>
2022-10-20 00:13:11 +0900

    Mapと現在地の表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,134 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
+
+
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Baby Names',
+      title: 'Map',
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
   @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+
 class _MyHomePageState extends State<MyHomePage> {
+  Position? currentPosition;
+  late GoogleMapController _controller;
+  late StreamSubscription<Position> positionStream;
+  late LatLng _initialPosition;
+  late bool _loading;
+
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
+    distanceFilter: 100,
+  );
+
   @override
+  void initState() {
+    super.initState();
+
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if(permission == LocationPermission.denied){
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
+      }
+    });
+
+    _loading = true;
+    _getUserLocation();
+  }
+
+
+
+  void _getUserLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      _loading = false;
+      print(position);
+    });
+  }
+
+
+
+  //現在位置を更新し続ける
+  void positionstrem() {
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+          currentPosition = position;
+          print(position == null
+              ? 'Unknown'
+              : '${position.latitude.toString()}, ${position.longitude
+              .toString()}');
+        });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Map')),
-      body: _buildBody(context),
+      body: _loading
+          ? CircularProgressIndicator()
+          : SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+
+            GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: _initialPosition,
+                zoom: 17,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller = controller;
+              },
+              onTap: (LatLng latLang) {
+                print('Clicked: $latLang');
+              },
+              myLocationEnabled: true,
+
+            ),
+            //buildFloatingSearchBar(),
+          ],
+        ),
+      ),
     );
   }
 
+
+
   Widget _buildBody(BuildContext context) {
-    // return _buildList(context, dummySnapshot);
     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('toire').snapshots(), // babyは各々のコレクションIDに変更してください
+      stream: FirebaseFirestore.instance.collection('toire').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
@@ -50,34 +140,17 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     return ListView(
       padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+      //children: snapshot.map((data) => _buildListMap(context, data)).toList(),
+      //children: snapshot.map((data) => _buildListMap(context, data)).toList(),
     );
   }
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
-
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          subtitle: Text(record.ido.toString(),),
-          trailing: Text(record.keido.toString(),),
-
-
-          //onTap: () => record.reference.update({'votes': FieldValue.increment(1)}),
-        ),
-      ),
-    );
-  }
+//Widget build(BuildContext context, DocumentSnapshot data) {
+// Widget _buildListMap(BuildContext context, DocumentSnapshot data) {
+//   final record = Record.fromSnapshot(data);
+//   //       );
+// }
 }
-
 class Record {
   final String name;
   final double ido;
@@ -90,11 +163,11 @@ class Record {
       : assert(map['name'] != null),
         assert(map['ido'] != null),
         assert(map['keido'] != null),
-  //assert(map['idokeido'] != null),
+  // idokeido = map['idokeido'];
         name = map['name'],
         ido = map['ido'],
         keido = map['keido'];
-  // idokeido = map['idokeido'];
+  //idokeido = map['idokeido'];
   Record.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data() as Map<String, dynamic>, reference: snapshot.reference);
 
